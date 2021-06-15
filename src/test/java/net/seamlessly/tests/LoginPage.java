0ceb852d074a837553555686ce09b25ed5cd80f0
Benjamin <bnymnpinar@gmail.com>
2021-06-15 18:21:09 +0300

    Login method is created

--- src/test/java/net/seamlessly/tests/LoginPage.java
+++ src/test/java/net/seamlessly/tests/LoginPage.java
@@ -4,13 +4,22 @@ import net.seamlessly.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.Test;
+
 
 public class LoginPage {
 WebDriver driver = WebDriverFactory.getDriver("chrome");
-    WebElement usernameInputBox = driver.findElement(By.cssSelector("#user"));
-    WebElement passwordInputBox = driver.findElement(By.cssSelector("password"));
-    WebElement loginButton = driver.findElement(By.cssSelector("submit-form"));
+
+
+    public static void LoginSeamlessly(String username, String password, String browserType){
+        WebDriver driver = WebDriverFactory.getDriver(browserType);
+        WebElement usernameInputBox = driver.findElement(By.cssSelector("#user"));
+        WebElement passwordInputBox = driver.findElement(By.cssSelector("password"));
+        WebElement loginButton = driver.findElement(By.cssSelector("submit-form"));
+
+        usernameInputBox.sendKeys(username);
+        passwordInputBox.sendKeys(password);
+        loginButton.click();
+    }
 
 
 
