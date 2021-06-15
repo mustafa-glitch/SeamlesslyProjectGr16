81f73a3d8e5e1cff9f6c5b079f1337ad0665a7d9
Benjamin <bnymnpinar@gmail.com>
2021-06-15 18:46:32 +0300

    Login page elements located

--- src/test/java/net/seamlessly/tests/LoginPage.java
+++ src/test/java/net/seamlessly/tests/LoginPage.java
@@ -7,19 +7,14 @@ import org.openqa.selenium.WebElement;
 
 
 public class LoginPage {
-WebDriver driver = WebDriverFactory.getDriver("chrome");
 
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
 
-    public static void LoginSeamlessly(String username, String password, String browserType){
-        WebDriver driver = WebDriverFactory.getDriver(browserType);
         WebElement usernameInputBox = driver.findElement(By.cssSelector("#user"));
-        WebElement passwordInputBox = driver.findElement(By.cssSelector("password"));
-        WebElement loginButton = driver.findElement(By.cssSelector("submit-form"));
+        WebElement passwordInputBox = driver.findElement(By.cssSelector("#password"));
+        WebElement loginButton = driver.findElement(By.cssSelector("#submit-form"));
 
-        usernameInputBox.sendKeys(username);
-        passwordInputBox.sendKeys(password);
-        loginButton.click();
-    }
+
 
 
 
