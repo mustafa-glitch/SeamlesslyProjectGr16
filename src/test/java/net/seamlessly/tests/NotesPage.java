b2a8e89539c0663f1a61572bed1259e0882f5cd9
Benjamin <bnymnpinar@gmail.com>
2021-06-15 18:02:39 +0300

    NotesModule element is located

--- src/test/java/net/seamlessly/tests/NotesPage.java
+++ src/test/java/net/seamlessly/tests/NotesPage.java
@@ -1,4 +1,13 @@
 package net.seamlessly.tests;
 
+import net.seamlessly.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class NotesPage {
+    WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+    WebElement NotesModule = driver.findElement(By.xpath("//div[@id='apps']//li[contains(@data-id,'notes')]"));
+
 }
